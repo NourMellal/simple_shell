dab0c06f34e9d880fb4d563e5ec5907d5c8139b8
NourMellal <mellal.nour9@gmail.com>
2023-05-15 05:35:49 +0100

    more tools for string

--- main.h
+++ main.h
@@ -21,11 +21,14 @@ typedef struct cname
 
 char *read_line(void);
 
+/* string tools */
 int str_len(char *s);
 void str_rev(char s[]);
 int _stb(char *s, char *buf);
 int _strcmp(const char *s1, const char *s2);
-
+void write_fd(int fd, char *s)
+void *_memcpy(void *dest, const void *src, size_t n);
+char *_strdup(const char *s);
 
 
 #endif /* MIAN_H */
---
+++ strtools2.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * write_fd - Writes a string to a file descriptor
+ * @fd: The file descriptor to write to
+ * @s: The string to write
+ */
+void write_fd(int fd, char *s)
+{
+	if (!s)
+		s = "(null)";
+
+	int length = str_len(s);
+
+	write(fd, s, length);
+}
+
+/**
+ * _strdup - Duplicates a string
+ * @s: The string to duplicate
+ * Return: Pointer to the duplicated string, or NULL if fails
+ */
+char *_strdup(const char *s)
+{
+	int i;
+	char *result;
+
+	result = malloc(sizeof(char) * (_strlen((char *)s) + 1));
+	if (result == NULL)
+		return (NULL);
+
+	for (i = 0; s[i]; i++)
+		result[i] = s[i];
+
+	result[i] = '\0';
+
+	return (result);
+}
