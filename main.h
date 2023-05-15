1cab7bb3010c7385aa7f67fc63d04ed1cd09bacf
NourMellal <mellal.nour9@gmail.com>
2023-05-15 04:15:39 +0100

    starting point

---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* including files for shell project */
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+#define B_SIZE 1024
+
+typedef struct cname
+{
+	char *name;
+	void (*fnc)(char **);
+} cmd;
+
+char *read_line(void);
+
+#endif /* MIAN_H */
---
+++ stdio.c
@@ -0,0 +1,26 @@
+#include "main.h"
+ 
+//write me a function that reads a line from stdin
+
+char *read_line(void)
+{
+	char *str = NULL;
+	size_t len = 0;
+	ssize_t read_len;
+
+	do {
+		if (!(str = realloc(str, len + 256))) {
+			free(str);
+			return NULL;
+		}
+
+		read_len = getline(&str, &len, stdin);
+	} while (read_len < 0);
+
+	if (read_len == -1) {
+		free(str);
+		return NULL;
+	}
+
+	// null terminate
+}
