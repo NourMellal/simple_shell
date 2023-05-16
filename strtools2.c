a00a6f489b7f53ad68c424b9daa365edf9ab67c4
NourMellal <mellal.nour9@gmail.com>
2023-05-17 00:49:34 +0100

    strlen

--- strtools2.c
+++ strtools2.c
@@ -39,3 +39,17 @@ char *_strdup(const char *s)
 	return (result);
 }
 
+/**
+ * _strlen - length of a given string
+ * @s: the string
+ * Return: the length of given string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+
+	return (len);
+}
