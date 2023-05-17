eef41628522ef9e14fbdcc8364ecc1351cc125f3
maverick <55205houssam@gmail.com>
2023-05-17 19:13:46 +0100

    ctrl-c

---
+++ signals.c
@@ -0,0 +1,16 @@
+#include <signal.h>
+#include <unistd.h>
+
+void _handle_signal (int sig)
+{
+	(void)sig;
+	write(1, "CTRL-C\n", 7);	
+}
+int main(void)
+{
+	signal(SIGINT, _handle_signal);
+	while (1)
+	{
+		sleep (1);
+	}
+}
